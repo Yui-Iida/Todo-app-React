5c748187277a0459871214c67cb18a40993cb96e
Yui <yxuxi328@icloud.com>
2022-10-13 13:27:23 +0200

    add theme

--- src/components/Important.js
+++ src/components/Important.js
@@ -6,28 +6,15 @@ const Important = ({ todo, setTodo, updateStatus }) => {
   const [theme] = useTheme();
 
   return (
-<<<<<<< HEAD
     <div className={`importantcontent-${theme}`}>
       <Text p="2" fontSize={{ base: '30px', md: '42px', lg: '55px' }}>
-=======
-    <div className={styles.important}>
-<<<<<<< HEAD
-      <Text p="2" fontSize={{ base: '32px', md: '45px', lg: '60px' }}>
->>>>>>> ef9e53d302cb42d2dccf68e0c02d8cd0c5733b0a
         Important
       </Text>
-=======
-      <h2 className={styles.importantTitle}>Important</h2>
->>>>>>> 9a10ad3c7e76e3d028b8bfa01025755b61e1b99b
       {todo &&
         todo.map(todo => {
           if (todo && todo.status === 'important')
             return (
-<<<<<<< HEAD
               <HStack justify="center">
-=======
-              <HStack justifyContent="center">
->>>>>>> 9a10ad3c7e76e3d028b8bfa01025755b61e1b99b
                 <IconButton
                   // onClick={() => complete(todo.id)}
                   icon={<VscCheck />}
@@ -37,16 +24,12 @@ const Important = ({ todo, setTodo, updateStatus }) => {
                   key={todo.id}
                   onClick={() => updateStatus(todo.id, 'did')}
                 />
-<<<<<<< HEAD
                 <Text
                   key={todo.id}
                   // fontSize="30"
                   color="white"
                   fontSize={{ base: '20px', md: '40px', lg: '56px' }}
                 >
-=======
-                <Text key={todo.id} fontSize="30" color="white">
->>>>>>> 9a10ad3c7e76e3d028b8bfa01025755b61e1b99b
                   {todo.content}
                 </Text>
               </HStack>
--- src/components/Keep.js
+++ src/components/Keep.js
@@ -6,19 +6,10 @@ const Keep = ({ todo, setTodo, updateStatus }) => {
   const [theme] = useTheme();
 
   return (
-<<<<<<< HEAD
     <div className={`keepcontent-${theme}`}>
       <Text p="1" fontSize={{ base: '20px', md: '30px', lg: '35px' }}>
-=======
-    <div className={styles.keep}>
-<<<<<<< HEAD
-      <Text p="1" fontSize={{ base: '25px', md: '30px', lg: '35px' }}>
->>>>>>> ef9e53d302cb42d2dccf68e0c02d8cd0c5733b0a
         Keep in mind
       </Text>
-=======
-      <h1 className={styles.keepTitle}>Keep in mind</h1>
->>>>>>> 9a10ad3c7e76e3d028b8bfa01025755b61e1b99b
       {todo &&
         todo.map(todo => {
           if (todo && todo.status === 'keep')
@@ -34,14 +25,10 @@ const Keep = ({ todo, setTodo, updateStatus }) => {
                   key={todo.id}
                   onClick={() => updateStatus(todo.id, 'did')}
                 />
-<<<<<<< HEAD
                 <Text
                   key={todo.id}
                   fontSize={{ base: '15px', md: '25px', lg: '30px' }}
                 >
-=======
-                <Text key={todo.id} fontSize="20">
->>>>>>> 9a10ad3c7e76e3d028b8bfa01025755b61e1b99b
                   {todo.content}
                 </Text>
               </HStack>
